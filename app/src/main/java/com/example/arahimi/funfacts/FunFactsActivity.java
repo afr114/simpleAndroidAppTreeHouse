357b7a1d057e039a2d337ea37841631577491f4e
Abdul Rahimi <afr114@gmail.com>
2015-12-01 16:26:14 -0800

    Treehouse Simple Android App

---
+++ app/src/main/java/com/example/arahimi/funfacts/FunFactsActivity.java
@@ -0,0 +1,39 @@
+package com.example.arahimi.funfacts;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Button;
+import java.util.Random;
+
+public class FunFactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Declare our View variables and assign the views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fact = "";
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.nextInt(3);
+                fact = randomNumber + "";
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+    }
+}
