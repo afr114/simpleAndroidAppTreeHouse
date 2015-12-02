02f405b46150cc6b9cd3d5f5d3f3d131e94193d4
Abdul Rahimi <afr114@gmail.com>
2015-12-01 19:08:26 -0800

    add classes and update styling

--- app/src/main/java/com/example/arahimi/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/arahimi/funfacts/FunFactsActivity.java
@@ -1,5 +1,6 @@
 package com.example.arahimi.funfacts;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,29 +9,36 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Button;
 import java.util.Random;
 
+
 public class FunFactsActivity extends AppCompatActivity {
+
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
 
+
+
         // Declare our View variables and assign the views from the layout file
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
-        Button showFactButton = (Button) findViewById(R.id.showFactButton);
-
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout funFactsLayout = (RelativeLayout) findViewById(R.id.funFactsLayout);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String fact = "";
-                Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(3);
-                fact = randomNumber + "";
+                String fact = mFactBook.getFact();
+                int color = mColorWheel.getColor();
                 factLabel.setText(fact);
+                funFactsLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
             }
         };
         showFactButton.setOnClickListener(listener);
